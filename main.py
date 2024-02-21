7a8c0f26d20df0114bfe4c21fe762add1f722906
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2024-02-21 11:38:57 -0600

    example

---
+++ main.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2) # this is a super long comment that should hopefully get cought by the black formatter
+
+if __name__ == "__main__":
+  print(fibonacci(10))
+  
